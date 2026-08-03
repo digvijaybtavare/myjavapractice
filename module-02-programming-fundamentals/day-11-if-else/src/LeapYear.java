7faa1a3ea2378144e57f2d44f8cde7ddbc09efd7
Digvijay <digvijay_tavare@yahoo.in>
2026-08-03 06:31:01 +0530

    Add leap year checker using if-else

---
+++ module-02-programming-fundamentals/day-11-if-else/src/LeapYear.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class LeapYear {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        //if (a%100!=0 &&(a%4==0))
+
+        if (a%4==0){
+            System.out.println("Leap Year");
+        }
+        else if ((a%4==0 && a%100==0) || (a%400!=0)){
+            System.out.println("Leap Year");
+        }
+        else {
+            System.out.println("Not a Leap Year");
+        }
+
+        sc.close();
+        
+    }
+    
+}
