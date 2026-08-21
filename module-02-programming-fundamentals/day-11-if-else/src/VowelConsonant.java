d7018c0df24eb7c46e17ab728e522674924bcaf4
Digvijay <digvijay_tavare@yahoo.in>
2026-08-21 14:51:44 +0530

    Add vowel or consonant problem

---
+++ module-02-programming-fundamentals/day-11-if-else/src/VowelConsonant.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class VowelConsonant {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+
+        if (a=='a' || a=='e' || a=='i' || a=='o' || a=='u' ||
+            a=='A' || a=='E' || a=='I' || a=='O' || a=='U') {
+                System.out.println("Vowel");
+            }
+
+        else System.out.println("Consonant");
+
+
+
+        sc.close();
+    }
+    
+}
