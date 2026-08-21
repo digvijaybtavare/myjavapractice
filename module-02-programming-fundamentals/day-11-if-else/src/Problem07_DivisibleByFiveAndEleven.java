9f8976630c2d0f59cb9f3376cb09cdd3eef2e565
Digvijay <digvijay_tavare@yahoo.in>
2026-08-21 15:41:49 +0530

    Add Problem 07 - Divisible by 5 and 11

---
+++ module-02-programming-fundamentals/day-11-if-else/src/Problem07_DivisibleByFiveAndEleven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Problem07_DivisibleByFiveAndEleven {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a%5==0 && a%11==0){
+            System.out.println("Divisible by both 5 and 11");
+        }
+        else System.out.println("Not divisible by both 5 and 11");
+
+
+        sc.close();
+    }
+    
+}
